cbfa5328d19b85ae337c70859fd036b2df5ed114
ArturMamyrasulov01 <arturmamyrasulov@gmail.com>
2024-05-04 22:22:31 +0600

    flutter_vative_splash add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 import 'package:t_store_app/app.dart';
 
 main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(
     const App(),
   );
