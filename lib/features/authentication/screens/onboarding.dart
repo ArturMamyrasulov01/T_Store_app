4e3815f9744331eaec818175b29294195765155a
ArturMamyrasulov01 <arturmamyrasulov@gmail.com>
2024-05-05 20:43:37 +0600

    add

---
+++ lib/features/authentication/screens/onboarding.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            children: [
+              Column(
+                children: [
+                  Image(
+                    image: AssetImage(""),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -6,4 +6,13 @@ class TImages {
 //Social Logos
   static const String google = "assets/logos/google-logo.png";
   static const String facebook = "assets/logos/logo-facebook.png";
+
+  //OnBoardingImages
+
+  static const String onBoardingImage1 =
+      "assets/images/on_boarding_images/sammy-line-sailor-on-mast-looking-through-telescope.gif";
+  static const String onBoardingImage2 =
+      "assets/images/on_boarding_images/sammy-line-woman-on-skateboard-with-file-folders.gif";
+  static const String onBoardingImage3 =
+      "assets/images/on_boarding_images/sammy-line-man-and-dog-delivering-packages-on-a-moped.gif";
 }
